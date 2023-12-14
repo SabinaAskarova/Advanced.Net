ccb5a4f096fceac69461c391a6a53aa085c6d894
Murat Vuranok <murat.vuranok@code.local>
2023-12-14 13:30:20 +0400

    domge,collection

---
+++ Unit/Lesson6/WorkingWithArrays/Tasks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+string[] cities =
+{
+"New York",
+"Paris",
+"London",
+"Sydney",
+"Cairo",
+"London",
+"Moscow",
+"Toronto",
+"Mumbai",
+"Rome"
+};
+//if(Array.IndexOf(cities,"London")!=Array.LastIndexOf(cities,"London")) Console.WriteLine("iki defe tekrarlanib");
+//for (int i = 0; i < cities.Length; i++)
+//{
+//    for (int j = 0; j < cities.Length; j++)
+//    {
+//        if (cities[i] == cities[j] && i!=j)
+//        {
+//            Console.WriteLine(cities[i]+" iki defe tekrarlanib");
+//        }
+//    }
+//}
+
+string olke = "Azerbaycan".ToLower();
+//Console.WriteLine(olke.Length);
+for (int i = 0; i < olke.Length; i++)
+{
+    //int counter = 1;
+    //for (int j = 0; j < olke.Length; j++)
+    //{
+    //    if (olke[i] == olke[j] && i != j) { counter++; }
+    //}
+    //Console.WriteLine(olke[i]+ " sayi: "+counter);
+
+    Console.WriteLine(olke[i] + " sayi: "+olke.Where(c => c == olke[i]).Count()); ;
+}
+
+var olkee = olke.Select((value) => (value));
+var result = olke.Where(c => c ==  (olke.Select((value) => (value))).Count());
+Console.WriteLine();
---
+++ Unit/Lesson6/WorkingWithArrays/WorkingWithArrays/Program.cs
@@ -0,0 +1,94 @@
+﻿
+//void Simple1()
+//{
+//    //1
+//    string[] sehirler = new string[5];
+
+//}
+
+//Simple1();
+
+//void Simple2()
+//{
+//    string[] cars = new string[10];
+
+//    cars[0] = "Ferrari";
+//    cars[1] = "BMW";
+//    cars[2] = "Mercedes";
+//    cars[3] = "Toyota";
+//    cars[4] = "Nissan";
+//    cars[5] = "Kia";
+//    cars[6] = "Toyota Prius";
+//    Random random = new Random();
+//    int index= random.Next(7);
+//    Console.WriteLine(cars[index]);
+//}
+
+//Simple2();
+
+
+//void Cars(){
+
+
+//}
+
+//List<string> cities = new List<string>() { "Ankara", "İstanbul" };
+//cities.Add("Bursa");
+//cities.Add("İzmir");
+//cities.Add("Antalya");
+//cities.Add("Adana");
+//cities.Add("Trabzon");
+//cities.Add("Samsun");
+//cities.Add("Konya");
+//cities.Add("Kayseri");
+//cities.Add("Kocaeli");
+//cities.Add("Diyarbakır");
+//cities.Add("Gaziantep");
+//cities.Add("Şanlıurfa");
+//cities.Add("Mersin");
+//cities.Add("Malatya");
+//cities.Add("Kahramanmaraş");
+//cities.Add("Erzurum");
+//cities.Add("Eskişehir");
+
+//// foreach ile 
+//int index=0;
+//foreach(var item in cities){
+//    //index-> item.value
+//    System.Console.WriteLine(item.IndexOf(item)+" -> "+item);
+//}
+
+
+
+
+List<string> cities = new List<string>() { "Ankara", "İstanbul" };
+cities.Add("Bursa");
+cities.Add("İzmir");
+cities.Add("Antalya");
+cities.Add("Adana");
+cities.Add("Trabzon");
+cities.Add("Samsun");
+cities.Add("Konya");
+cities.Add("Kayseri");
+cities.Add("Kocaeli");
+cities.Add("Diyarbakır");
+cities.Add("Gaziantep");
+cities.Add("Şanlıurfa");
+cities.Add("Mersin");
+cities.Add("Malatya");
+cities.Add("Kahramanmaraş");
+cities.Add("Erzurum");
+cities.Add("Eskişehir");
+
+// foreach ile 
+foreach (var item in cities)
+{
+    //index-> item.value
+    Console.WriteLine(cities.IndexOf(item) + " -> " + item);
+}
+
+foreach (var item in cities.Select((value,index)=>(value,index)))
+{
+    //index-> item.value
+    Console.WriteLine(item.index + " -> " + item.value);
+}
